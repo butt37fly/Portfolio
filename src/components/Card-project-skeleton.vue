6940e9ea50ae3733b368a2e95479f283647a484c
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-03 21:50:43 -0500

    refactor: eliminar código innecesario

--- src/components/Card-project-skeleton.vue
+++ src/components/Card-project-skeleton.vue
@@ -1,7 +1,4 @@
 <script setup>
-import IconDefault from '@/components/Icon-default.vue'
-import TagDefault from '@/components/Tag-default.vue'
-
 const getRandomImgHeight = () => {
   const max = 120
   const min = 300
@@ -11,45 +8,19 @@ const getRandomImgHeight = () => {
 </script>
 
 <template>
-  <article
-    v-if="onModal == true"
-    class="c-card c-card--modal u-flex u-align-start u-justify-start u-w-100"
-  >
-    <div class="c-card__content u-flex u-w-100 u-p-m u-gap-m">
-      <div class="c-card__heading u-flex u-w-100 u-align-start u-gap-s">
-        <h3 class="c-card__title subtitle">{{ props.title }}</h3>
-        <div class="c-card__details u-flex u-row u-gap-s">
-          <span v-show="props.date != ''" class="c-card__detail u-flex u-row u-gap-1">
-            <IconDefault name="clock" />
-            {{ props.date }}
-          </span>
-          <span v-show="props.at != ''" class="c-card__detail u-flex u-row u-gap-1">
-            <IconDefault name="link" />
-            {{ props.at_text }}
-            <a :href="props.at_url" target="_blank"> {{ props.at }}</a>
-          </span>
-        </div>
-        <div class="c-card__tags u-flex u-row u-align-start u-justify-start u-w-100 u-pb-2 u-gap-2">
-          <TagDefault v-for="item in props.tags" :key="item" :name="item" />
-        </div>
-      </div>
-      <div :v-html="props.content" class="c-card__text"></div>
-    </div>
-  </article>
-
-  <article v-else class="c-card-skeleton u-flex u-align-start u-justify-start u-w-100">
+  <article class="c-card-skeleton u-flex u-align-start u-justify-start u-w-100">
     <div
       class="c-card-skeleton__image u-skeleton"
       :style="`--heigth: ${getRandomImgHeight()}px;`"
     ></div>
-    <div class="c-card-skeleton__content u-flex u-w-100 u-p-m u-gap-m">
-      <div class="c-card-skeleton__heading u-flex u-row u-w-100 u-align-end u-gap-1">
+    <div class="c-card-skeleton__content u-flex u-w-100 u-p-4 u-g-4">
+      <div class="c-card-skeleton__heading u-flex u-row u-w-100 u-align-end u-g-1">
         <h3 class="c-card-skeleton__title u-skeleton subtitle"></h3>
-        <div class="c-card-skeleton__details u-skeleton u-flex u-row u-gap-s">
-          <span class="c-card-skeleton__detail u-flex u-row u-gap-1"> </span>
+        <div class="c-card-skeleton__details u-skeleton u-flex u-row u-g-3">
+          <span class="c-card-skeleton__detail u-flex u-row u-g-1"> </span>
         </div>
       </div>
-      <div class="c-card-skeleton__tags u-flex u-row u-align-start u-justify-start u-w-100 u-gap-2">
+      <div class="c-card-skeleton__tags u-flex u-row u-align-start u-justify-start u-w-100 u-g-2">
         <span class="c-card-skeleton__tag u-skeleton"></span>
         <span class="c-card-skeleton__tag u-skeleton"></span>
         <span class="c-card-skeleton__tag u-skeleton"></span>
@@ -88,19 +59,6 @@ const getRandomImgHeight = () => {
   &__tag {
     width: 80px;
     height: 24px;
-  }
-}
-
-.c-card--modal {
-  border: unset;
-  height: 100%;
-
-  &__image {
-    cursor: default;
-  }
-
-  &:hover {
-    filter: unset;
   }
 }
 </style>
